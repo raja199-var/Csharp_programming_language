2dff57f59ddb25a6f600e1eeb94f9f6517586075
raja199-var <129877209+raja199-var@users.noreply.github.com>
2023-04-04 08:31:07 -0700

    Add files via upload

---
+++ MethodOverriding/MethodOverriding/MethodOverriding/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverriding
+{
+     class Program
+    {
+      public virtual void test()
+        {
+            Console.WriteLine("Hello Parent");
+        }
+
+    }
+     class Demo : Program
+    {
+        public override void test()
+        {
+            Console.WriteLine("Hello child");
+        }
+    
+        
+
+    }
+    class Result
+    {
+        public static void Main(string[] args)
+        {
+            Demo de = new Demo();
+            de.test();
+
+            Program p = new Program();
+            p.test();
+
+            Program p1 = new Demo();
+            p1.test();
+            Console.ReadLine();
+        }
+    }
+}
