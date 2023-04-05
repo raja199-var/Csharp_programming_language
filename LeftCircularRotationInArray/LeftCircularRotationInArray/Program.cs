d07b16fcefbda49df576d3b49bfdca1963b6c66f
raja199-var <129877209+raja199-var@users.noreply.github.com>
2023-04-05 08:43:20 -0700

    Add files via upload

---
+++ LeftCircularRotationInArray/LeftCircularRotationInArray/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeftCircularRotationInArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the size of the array: ");
+            int size = int.Parse(Console.ReadLine());
+            int[] arr = new int[size];
+            Console.WriteLine("Enter the elements: ");
+            for(int i = 0; i < size; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+
+            }
+            int x = arr[0];
+            for (int i = 1; i <= size-1; i++)
+            {
+                arr[i-1] =arr[i];
+            }
+            arr[size - 1] = x;
+          
+           
+
+            Console.WriteLine("Resulted array is: ");
+            for(int i = 0; i < size; i++)
+            {
+                Console.Write(arr[i] + " ");
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PossibleSubstringsOfAString/PossibleSubstringsOfAString/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * program to find all substrings of a string using Substring() method
+ * 
+ * */
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PossibleSubstringsOfAString
+{
+    internal class Program
+    {
+         static void Main(string[] args)
+         {
+             Console.WriteLine("Enter the string: ");
+             string str = Console.ReadLine();
+         
+            
+             for(int  i =1;i<=str.Length;i++)
+             {
+                 for(int j = 0; j <=str.Length-i; j++)
+                 {
+                     string substring = str.Substring(j,i) ;
+                     Console.WriteLine(substring);
+                 }
+             }
+
+             Console.ReadLine();
+         }
+   
+    }
+}
