c449339ed4e96b788263c73697b017e198767bf4
raja199-var <129877209+raja199-var@users.noreply.github.com>
2023-04-06 08:43:25 -0700

    Add files via upload

---
+++ ConstructorChaining/ConstructorChaining/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Program to show the concept of constructor chaining i.e order of default constructors
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorChaining
+{
+    internal class Program
+    {
+        public Program()
+        {
+            Console.WriteLine("Hello GrandParent");
+        }
+    }
+    class Program1:Program
+    {
+        public Program1()
+        {
+            Console.WriteLine("Hello Parent");
+        }
+
+    }
+    class Program2 : Program1
+    {
+        public Program2()
+        {
+            Console.WriteLine("Hello child");
+        }
+    }
+       
+    class Program3
+    {
+        static void Main(string[] args)
+        {
+            Program2 p = new Program2();
+            Console.ReadLine();
+            
+        }
+        
+    }
+        
+          
+}
---
+++ ConstructorChainingOfParameterizedConstructors/ConstructorChainingOfParameterizedConstructors/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorChainingOfParameterizedConstructors
+{
+    internal class Program
+    {
+       public Program(int x)
+        {
+            Console.WriteLine("x"+x);
+        }
+
+    }
+    class Program1:Program
+    {
+        public Program1(int y):base(20)
+        {
+            Console.WriteLine("y"+y);
+        }
+
+    }
+    class Program2:Program1
+    {
+        public Program2(int z):base(30)
+        {
+            Console.WriteLine("z"+z);
+
+        }
+    }
+    class Program3
+    {
+        public static void Main(string[] args)
+        {
+            Program2 p = new Program2(10);
+            Console.ReadLine();
+        }
+    }
+}
